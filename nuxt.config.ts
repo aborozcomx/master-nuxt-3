cdcdbd8400c0291092a2b181edcbd65e13f34afb
Adrian Bautista <adrianbautistaorozco@gmail.com>
2023-07-04 18:04:40 -0500

    :sparkles: Add landing page & components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      routes: ['/landing'],
+    },
+  },
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxtjs/supabase', '@pinia/nuxt']
 })
